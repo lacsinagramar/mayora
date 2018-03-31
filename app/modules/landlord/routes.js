a285436bcbe02b612bb4d34694bf7c86c61f3abb
lacsinagramar <lacsinagramar@gmail.com>
2018-04-01 05:33:08 +0800

    add edit rooms

--- app/modules/landlord/routes.js
+++ app/modules/landlord/routes.js
@@ -9,4 +9,15 @@ router.get('/rooms', (req,res) =>{
     res.render('landlord/views/rooms');
 });
 
+router.post('/addroom', (req, res) =>{
+    console.log('wala pa mare\n')
+    console.log(req.body);
+});
+
+router.post('/editroom', (req, res) =>{
+    console.log('wala pa mare\n')
+    console.log(req.body);
+    res.send(true);
+});
+
 exports.landlord = router;
